2aa131e8fff4dd671bd1bc5224cdcb9d4d999ebf
Yslelf <yslelf714@outlook.com>
2026-09-09 23:31:34 +0800

    ci: 启用 GitHub Pages 发布

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ import { useTransformByRules, type TransformRule } from "./theme/composables/use
 
 // 🔗 国际化配置 - 激活多语言支持
 export default defineConfig({
-  base: '/',  // 改成你的仓库名
+  base: '/CloudPickDocs/',
   ...shared,
   locales: {
     root: { label: "简体中文", ...zh }, // 默认中文语言
